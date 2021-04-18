58afbf17ce987f407b82b1ca00e1448ca8b7c3ff
miladhzz <miladhzz@gmail.com>
2021-04-18 22:50:21 +0430

    init model and use tinymce on admin

--- djangotinymce/settings.py
+++ djangotinymce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'texteditor',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
--- texteditor/models.py
+++ texteditor/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = HTMLField()
