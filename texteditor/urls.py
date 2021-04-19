d8bd425046e6e066a571eaf921b6b8a39b82b018
m.hatami <m.hatami@son.ir>
2021-04-19 19:05:07 +0430

    add detail view

--- texteditor/urls.py
+++ texteditor/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name="index"),
+    path('post/<int:pk>/', views.Detail.as_view(), name="detail"),
 ]
--- texteditor/views.py
+++ texteditor/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from . import forms
+from django.shortcuts import reverse
+from . import models
 
 
 class Index(generic.CreateView):
     form_class = forms.PostForm
     template_name = "index.html"
-    success_url = "/"
+
+    def get_success_url(self):
+        return reverse('detail', args=[self.object.id])
+
+
+class Detail(generic.DetailView):
+    model = models.Post
+    template_name = "detail.html"
