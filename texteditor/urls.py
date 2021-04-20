de92efae1ac21ddcb689ecb347a6da2889c74062
m.hatami <m.hatami@son.ir>
2021-04-20 07:40:14 +0430

    Revert "add detail view"
    This reverts commit d8bd4250

--- texteditor/urls.py
+++ texteditor/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.Index.as_view(), name="index"),
-    path('post/<int:pk>/', views.Detail.as_view(), name="detail"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
