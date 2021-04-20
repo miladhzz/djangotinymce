f765f840de08de7bfa77efd8dc568fbd92505340
m.hatami <m.hatami@son.ir>
2021-04-20 07:43:57 +0430

    serve static and media files

--- djangotinymce/settings.py
+++ djangotinymce/settings.py
@@ -118,5 +118,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
